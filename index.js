9016def43996934d3ef8154f6ec1ba5b7fdd6390
mcpicornell <marc.castro.picornell@gmail.com>
2023-05-22 21:06:23 +0200

    txt to csv in ts

--- index.js
+++ index.js
@@ -8,38 +8,22 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     return to.concat(ar || Array.prototype.slice.call(from));
 };
 var fs = require('fs');
-// const inputPath = 'countries.txt';
-// const outputPath = 'countries.csv';
-// const fileContent = fs.readFileSync(inputPath, 'utf-8');
-// const lines = fileContent.trim().split('\n');
-// const headers = ['Country', 'Population', 'Area', 'Density'];
-// type CountryData = [string, number, number, number];
-// const data: CountryData[] = lines.map((line: string) => {
-//   const values = line.trim().split(/\s+/);
-//   const country = values.slice(0, -2).join(' ');
-//   const population = parseFloat(values[values.length - 2].replace(/,/g, ''));
-//   const area = parseFloat(values[values.length - 1].replace(/,/g, ''));
-//   const density = population / area;
-//   return [country, population, area, density] as CountryData;
-// });
-// let csvContent = [headers.join(','), ...data.map((row) => row.join(','))].join('\n');
-// fs.writeFileSync(outputPath, csvContent, 'utf-8');
-// console.log('Archivo CSV generado exitosamente.');
 var inputPath = 'countries.txt';
 var outputPath = 'countries.csv';
 var fileContent = fs.readFileSync(inputPath, 'utf-8');
 var lines = fileContent.trim().split('\n');
 var headers = ['Country', 'Population', 'Area', 'Density'];
-var data = lines
-    .map(function (line) {
+var data = lines.map(function (line) {
     var values = line.trim().split(/\s+/);
     var country = values.slice(0, -2).join(' ');
     var population = parseFloat(values[values.length - 2].replace(/,/g, ''));
     var area = parseFloat(values[values.length - 1].replace(/,/g, ''));
     var density = population / area;
     return [country, population, area, density];
-})
-    .filter(function (row) { return !isNaN(row[1]) && !isNaN(row[2]) && !isNaN(row[3]); }); // Filtrar filas con valores NaN
-var csvContent = __spreadArray([headers.join(',')], data.map(function (row) { return row.join(','); }), true).join('\n');
+});
+// Filtrar la fila "Country,NaN,NaN,NaN"
+var filteredData = data.filter(function (row) { return !isNaN(row[1]) && !isNaN(row[2]) && !isNaN(row[3]); });
+// Ordenar por densidad en orden descendente
+filteredData.sort(function (a, b) { return b[3] - a[3]; });
+var csvContent = __spreadArray([headers.join(',')], filteredData.map(function (row) { return row.join(','); }), true).join('\n');
 fs.writeFileSync(outputPath, csvContent, 'utf-8');
-console.log('Archivo CSV generado exitosamente.');
--- index.ts
+++ index.ts
@@ -10,22 +10,24 @@ const headers = ['Country', 'Population', 'Area', 'Density'];
 
 type CountryData = [string, number, number, number];
 
-const data: CountryData[] = lines
-  .map((line: string) => {
-    const values = line.trim().split(/\s+/);
+const data: CountryData[] = lines.map((line: string) => {
+  const values = line.trim().split(/\s+/);
 
-    const country = values.slice(0, -2).join(' ');
-    const population = parseFloat(values[values.length - 2].replace(/,/g, ''));
-    const area = parseFloat(values[values.length - 1].replace(/,/g, ''));
+  const country = values.slice(0, -2).join(' ');
+  const population = parseFloat(values[values.length - 2].replace(/,/g, ''));
+  const area = parseFloat(values[values.length - 1].replace(/,/g, ''));
 
-    const density = population / area;
+  const density = population / area;
 
-    return [country, population, area, density] as CountryData;
-  })
-  .filter((row: CountryData) => !isNaN(row[1]) && !isNaN(row[2]) && !isNaN(row[3])); 
+  return [country, population, area, density] as CountryData;
+});
 
-let csvContent = [headers.join(','), ...data.map((row) => row.join(','))].join('\n');
+// Filtrar la fila "Country,NaN,NaN,NaN"
+const filteredData = data.filter((row) => !isNaN(row[1]) && !isNaN(row[2]) && !isNaN(row[3]));
+
+// Ordenar por densidad en orden descendente
+filteredData.sort((a, b) => b[3] - a[3]);
+
+const csvContent = [headers.join(','), ...filteredData.map((row) => row.join(','))].join('\n');
 
 fs.writeFileSync(outputPath, csvContent, 'utf-8');
-
-console.log('Archivo CSV generado exitosamente.');
